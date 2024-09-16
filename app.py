04f86b5d624f2e031214c38634e9d581cdcef6d0
1301ek <basseypraise28@gmail.com>
2024-09-16 19:05:58 +0100

    Fixed bugs and redid frontend

--- app.py
+++ app.py
@@ -5,27 +5,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    jobs = load_jobs_from_db()
-    return render_template('home.html',jobs=jobs)
+    return render_template('index.html')
 
-@app.route("/api/jobs")
-def list_jobs():
+@app.route("/listings")
+def list():
     jobs = load_jobs_from_db()
-    return jsonify(jobs)
+    return render_template('listings.html',jobs=jobs)
 
 @app.route("/job/<id>")
 def show_job(id):
     job = load_job_from_db(id)
     if not job:
         return "Not Found", 404
-    return render_template('jobpage.html', job=job)
+    return render_template('jobinfo.html', job=job)
 
-@app.route("/job/<id>/apply", methods=['post'])
+@app.route("/job/<id>/apply")
+def application_form(id):
+    job = load_job_from_db(id)
+    return render_template('application_data.html', id=id, job=job)
+
+@app.route("/job/<id>/apply/submit", methods=['post'])
 def apply_to_job(id):
     data = request.form
     job = load_job_from_db(id)
     add_application_to_db(id, data)
-    return render_template('application_submitted.html', application = data, job = job)
+    return render_template('applysuccess.html', data = data, job = job)
     
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
